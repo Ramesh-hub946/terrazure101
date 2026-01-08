9bd7ea03ee196935659ea05e77ad3d6a8ae7efa2
Ramesh-hub946 <rameshparvathaneni1234@gmail.com>
2026-01-08 16:20:31 +0530

    m3

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "rg" {
   name = "Vcube-RG"
   location = "East US"
 }
+
+
+resource "azurerm_resource_group" "rg2" {
+  name = "Vcube-RG12"
+  location = "East US"
+}
